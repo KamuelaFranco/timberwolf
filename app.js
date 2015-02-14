37ada03621d16a1995e41d4a5f742f4a63c6705b
Kamuela Franco <kamuela.franco@gmail.com>
2015-02-14 07:59:14 +0000

    Reset app.js to original bittorrent-tracker example code

--- app.js
+++ app.js
@@ -1,41 +1,14 @@
-var app, btt, express, port, tracker, trackerPort;
+var Server = require('./').Server; //TODO: Fix this import, perhaps fix the module export in server.js
+var express = require('express');
+var app = express();
 
-port = process.env.PORT || 3000;
-
-trackerPort = process.env.PORT_TRACKER || 3001;
-
-btt = require('./server');
-
-tracker = new btt.Server({
-  udp: false,
-  http: true,
-  filter: function(infoHash) {
-    return infoHash;
-  }
+var server = new Server({
+  http: false, // we do our own
+  udp: false   // not interested
 });
 
-tracker.on('error', function(error) {
-  return console.log(error.message);
-});
+var onHttpRequest = server.onHttpRequest.bind(server);
+app.get('/announce', onHttpRequest);
+app.get('/scrape', onHttpRequest);
 
-tracker.on('warning', function(error) {
-  return console.log(error.message);
-});
-
-tracker.listen(trackerPort, function() {
-  return console.log("Tracker module listening on http port: " + (tracker.http.address().port));
-});
-
-express = require('express');
-
-app = express();
-
-app.get('/stats', function(req, res) {
-  console.log('GET /stats');
-  res.json('Stats');
-  return res.end;
-});
-
-app.listen(port, function() {
-  return console.log('Express listening on port ' + port);
-});
+app.listen(8080);
