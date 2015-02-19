f23d7581bd4a154e92797a6f1def807f86aa5411
Kamuela Franco <kamuela.franco@gmail.com>
2015-02-19 03:34:36 +0000

    Refactored validations to /lib/validations.js

--- app.js
+++ app.js
@@ -9,54 +9,7 @@ var bencode = require('bencode');
 var app = express();
 var common = require('./lib/common');
 var schedule = require('node-schedule');
-
-
-// Validation functions
-// TODO: Implement this DB check for users #user_validation
-isGoodUser = function (secret, callback) {
-	db.getUser(secret, function (reply) {
-		if (reply == null) {
-			callback('passkey not found');
-		}
-		else {
-			callback(true)
-		}
-	});
-};
-
-// TODO: Implement function for checking info_hash against DB #torrent_existence
-doesTorrentExist = function (infohash, callback) {
-	db.getTorrent(infohash, function (reply) {
-		if (reply == null) {
-			callback('Unregistered Torrent');
-		}
-		else {
-			callback(true)
-		}
-	});
-};
-
-// TODO: Implement function to check torrent client against blocked list #client_whitelisting
-isTorrentClientGood = function (torrentCient) {
-	return true;
-};
-
-clearToAnnounce = function (secret, infohash, torrentClient, callback) {
-	isGoodUser(secret, function (res) {
-		if (res != true) {
-			callback('Invalid passkey');
-			return false;
-		}
-		doesTorrentExist(infohash, function (res) {
-			if (res != true) {
-				callback('Unregistered torrent');
-				return false;
-			}
-			callback(true);
-		});
-	});
-
-};
+var validations = require('./lib/validations');
 
 // TODO: Better understand what's going on here
 var onHttpRequest = server.onHttpRequest.bind(server);
