30e3030db1c065dd8c27f6d5fbf388120a970724
Kamuela Franco <kamuela.franco@gmail.com>
2015-02-24 18:01:10 +0000

    Added exports to parse_http.js functions and began implementation of whitelist checks

--- routes.js
+++ routes.js
@@ -1,4 +1,4 @@
-var parseHttpRequest = require('./lib/parse_http');
+var parseHttp = require('./lib/parse_http');
 
 var common = require('./lib/common');
 var validations = require('./lib/validations');
@@ -10,36 +10,43 @@ var express = require('express');
 var router = express.Router();
 
 // TODO: Populate an actual list of user passkeys from memory or database
-var userPasskeys = [432423423,3243243243,2343243243,234423];
+var userPasskeys = ['432423423','3243243243','2343243243','234423'];
+var torrents = ['cb26218e255fd66c9af955cf755457d0a5f72891']
 
 // These routes are for torrent access
 
-// TODO: Implement 'secret' param
+// TODO: Implement 'secret' param for user authentication
 
 // Parameter to handle user authentication by checking it against userPasskeys
-router.param('secret', function(req, res, next, id) {
-    if(userPasskeys.indexOf(id) != -1) { // if the secret is found in userPasskeys
-        console.log('Authorized request: ' + id.toString())
+router.param('secret', function(req, res, next, id) { // id is type string
+    console.log(userPasskeys.indexOf(id));
+    if (userPasskeys.indexOf(id) !== -1) { // if the secret is found in userPasskeys
+        console.log('Authorized request: ' + id);
         next();
     } else {
-        res.end('bad request');
+        res.end('Invalid passkey');
     }
 });
 
 // Save parameters and return swarm
 router.get('/:secret/announce', function (req, res) {
+    // TODO: Fix req.params.info_hash check
     // TODO: Check torrent existence
-
-    // TODO: Return swarm
-
-    // TODO: Save queries to Redis
-    res.end();
+    console.log(req.params.info_hash + ' ' + typeof req.params.info_hash)
+    if (torrents.indexOf(req.params.info_hash) !== -1) {
+        // TODO: Return swarm
+        parseHttp.parseAnnounceRequest(req);
+        // TODO: Save params to cache
+    } else {
+        res.end('No such torrent on tracker');
+    }
 });
 
 // TODO: Implement scrape functionality
 // Return peer count in swarm
 router.get('/:secret/scrape', function (req, res) {
-    res.end('Scrape has not been implemented yet. Please /announce instead.');
+    parseHttp.parseScrapeRequest(req);
+    //res.end('Scrape has not been implemented yet. Please /announce instead.');
 });
 
 // TODO: Implement stats return
